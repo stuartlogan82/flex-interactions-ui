f3aba36137aa18d4f14c65b20c4de322b787f5f1
stuartlogan82 <slogan@twilio.com>
2018-11-01 14:38:03 +0000

    set up devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -44,7 +44,8 @@ module.exports = (env) => {
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-      contentBase: path.join(__dirname, 'public')
+      contentBase: path.join(__dirname, 'public'),
+      publicPath: '/dist/'
     }
   };
 };
